5d8fac39ddaee5344445f35c1e6712a8aad7c2d2
Jona <jona07072000@gmail.com>
2024-09-09 14:13:37 -0500

    Estructura básica de los componentes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TodoApp } from './components/TodoApp.jsx'
+import { ListApp } from './components/ListApp.jsx'
 import "../index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TodoApp/>
+    <ListApp/>
   </StrictMode>,
 )
