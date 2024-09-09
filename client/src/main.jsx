032b5757eee2c8fd10ed2c36146195e6d7cdcf08
Jona <jona07072000@gmail.com>
2024-09-09 14:03:54 -0500

    Creación del App principal frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { TodoApp } from './components/TodoApp.jsx'
+import "../index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TodoApp/>
   </StrictMode>,
 )
