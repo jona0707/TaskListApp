43da489afd387d81c0166a8cc4fea5d0837f3d2c
Jona <jona07072000@gmail.com>
2024-09-09 17:26:07 -0500

    [Backend] Correcciones y configuracion del servidor para actualizar la BDD correctamente

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
+const sequelize = require('./config/config');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// Sincronizar modelo y BDD
+sequelize.sync({ force: false })  // `force: false` no elimina las tablas existentes
+  .then(() => {
+    console.log('Base de datos sincronizada');
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error al sincronizar la base de datos:', err);
+  });
