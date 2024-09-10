cf00f0e3beac1261660d2b0d9500e178dbca2958
Jona <jona07072000@gmail.com>
2024-09-09 23:34:39 -0500

    Cambios de estilos footer para producción

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ export const Footer = () => {
   return (
     <>
       <p className="footer">
-        {/* Creada por <a href="https://github.com/jona0707">Jona</a>,  */}
-        Impulsada por <a href="https://render.com/">Render</a> y <a href="https://www.clever-cloud.com/">Clever Cloud</a>.
+        Creada por <a href="https://github.com/jona0707">Jona</a>, 
+        impulsada por <a href="https://render.com/">Render</a> y <a href="https://www.clever-cloud.com/">Clever Cloud</a>.
       </p>
     </>
   );
